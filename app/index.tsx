e26e1a33a6c72fd538fb7f5aaede8f14b685bcae
fernandop-dev <fernandopuertof@gmail.com>
2025-11-02 16:50:46 -0600

    Organización final del proyecto con selección manual de ejercicios

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
-import MenuPrincipal from "./screens/MenuPrincipal";
+import { registerRootComponent } from "expo";
+import Ej1Formulario from "./screens/Ej1Formulario";
 
-export default function Index() {
-  return <MenuPrincipal />;
+export default function App() {
+  // 🔹 Cambiá esta línea según el ejercicio que querás mostrar:
+  return <Ej1Formulario />;
 }
+
+registerRootComponent(App);
